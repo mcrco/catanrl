4f20b00ff1e3ee35f7d4ca07cf1e9215800632ab
mod3sty <marco.yang.ca@gmail.com>
2025-11-21 02:36:09 -0800

    bug fixes post refactor

--- src/catanrl/algorithms/ppo/__init__.py
+++ src/catanrl/algorithms/ppo/__init__.py
@@ -2,12 +2,10 @@
 Shared PPO helpers for both single-agent and multi-agent training regimes.
 """
 
-from . import buffers, trainer_sarl, trainer_marl, utils, central_critic, sarl_ppo, marl_ippo
+from . import buffers, utils, central_critic, sarl_ppo, marl_ippo
 
 __all__ = [
     "buffers",
-    "trainer_sarl",
-    "trainer_marl",
     "utils",
     "central_critic",
     "sarl_ppo",
